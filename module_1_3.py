9c9fa9c1efed6fbf0d53364f935c939672c69371
24122024 <arakelyantssasha@gmail.com>
2024-10-14 05:32:15 +0400

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+# Практическая работа по уроку "Динамическая типизация"
+name = 'Sasha'
+print(name)# Output name
+age = 36
+print(age)# Output age
+new_age = 4
+print(age + new_age) # Add ap age
+is_student = 36<40
+print(is_student) # Print value
